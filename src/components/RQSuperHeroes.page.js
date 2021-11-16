ab0cf19c3b7dae8d58ece6f6a0b61fc854f33f92
rudra pramanik <rudrahimangshoo2020.com>
2021-11-16 09:54:44 +0600

    11

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,34 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes');
+};
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    'super-heros',
+    fetchSuperHeroes,
+    {
+      // staleTime: 40000,
+      enabled: false,
+    }
+  );
+  console.log(isLoading, isFetching);
+  if (isLoading || isFetching) {
+    return <div>is loading.......</div>;
+  }
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <>
+      <h2>rq hero</h2>
+      <button onClick={refetch}>btn</button>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}> {hero.name}</div>;
+      })}
+    </>
+  );
+};
